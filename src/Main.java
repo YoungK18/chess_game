2b08238e3e17c07f50a1dec19467795f6530daca
YoungK <ericko2005131@gmail.com>
2023-05-08 00:16:29 -0400

    1

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        ChessBoard board = new ChessBoard();
-        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
